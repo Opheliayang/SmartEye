81082956d80b2b932d72907827bbab406fb9ff6f
ParkLee <739830473@qq.com>
2016-02-21 19:05:12 +0800

    add actionbar
    主界面加入actionbar，但缺少功能实现

--- app/src/main/java/com/parklee/smarteye/MainActivity.java
+++ app/src/main/java/com/parklee/smarteye/MainActivity.java
@@ -10,12 +10,15 @@ package com.parklee.smarteye;
  *
  * 拍照已实现
  */
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,37 @@ public class MainActivity extends Activity {
 
     public void StartCamera(View view) {
         startActivity(new Intent(this, CameraActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_actionbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openSearch() {
+
+    }
+
+    private void openSettings(){
+
     }
 
 }
